636152a06dfab0e4b1281c0db7297ca07876017d
sadewni <sadewchamathya@gmail.com>
2025-03-17 11:27:19 +0530

    company-profile

---
+++ app/types/projects.ts
@@ -0,0 +1,13 @@
+export interface Projects {
+    id: string;
+    title: string;
+    description: string;
+    status: 'Completed';
+    visibility: 'Visible' | 'Hidden';
+    privacy: 'Public' | 'Private';
+    imageUrl: string;
+    // progress: number;
+    // members: number;
+    duration: string;
+    completedDate: string;
+}
