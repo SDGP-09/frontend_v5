1a76dc74c6b15f68c5ab35be00ba024127dc5258
AyendriJayasekara <jayasekaraayendri@gmail.com>
2025-03-20 22:19:08 +0530

    - Page header text added - Three main navigation buttons added - Logout button added to the layout - Project directory created and tsx file added - project route added to navigation button

--- app/application/main-console/layout.tsx
+++ app/application/main-console/layout.tsx
@@ -3,9 +3,14 @@ import React from "react";
 export default function ConsoleLayout(  {children,}: Readonly<{children: React.ReactNode;}>){
     return (
             <div className="w-screen h-screen bg-black flex flex-col text-white justify-center items-center p-10">
-                <div className="w-full h-full bg-amber-300 flex flex-col">
+                <div className="w-full h-full  flex flex-col">
                     {children}
-                    <div className= "w-full h-20 bg-green-400"></div>
+                    <div className= "w-full h-20  text-2xl flex justify-end items-center">
+                        <button className= "mr-10 cursor-pointer">
+                            <i className="ri-logout-circle-line"></i>
+                            <span className= "ml-3">log out</span>
+                        </button>
+                    </div>
                 </div>
             </div>
 
--- app/application/main-console/page.tsx
+++ app/application/main-console/page.tsx
@@ -1,14 +1,46 @@
+"use client"
+import {useRouter} from "next/navigation";
+
 export default function MainConsole(){
+    const router = useRouter();
     return(
        <div className="w-full flex-1  flex flex-col">
 
-           <div className= "w-full h-1/3 bg-emerald-900"></div>
+           <div className= "w-full h-1/3  text-6xl flex flex-col justify-center items-center">
+               <p className="font-bold">SriLankas Largest Construction Platform</p>
+               <p className="font-light text-5xl" >Manage your project with confidence</p>
+           </div>
 
            <div className= "w-full flex-1  flex">
-               <div className="h-full flex-1 bg-amber-400"></div>
-               <div className="h-full flex-1 bg-green-700"></div>
-               <div className="h-full flex-1 bg-fuchsia-800"></div>
+               <div className="h-full flex-1  flex justify-center items-center text-8xl">
+                   <button className= "w-auto h-auto cursor-pointer " onClick={()=>router.push("/application/main-console/project")}>
+                       <div>
+                           <i className="ri-presentation-line"></i>
+                           <p className= "underline text-2xl">Manage projects</p>
+                       </div>
+                   </button>
+
+
+               </div>
+               <div className="h-full flex-1  flex justify-center items-center text-8xl">
+                   <button className="w-auto h-auto cursor-pointer">
+                       <div>
+                           <i className="ri-todo-line"></i>
+                           <p className="underline text-2xl">Manage Tenders</p>
+                       </div>
+                   </button>
+               </div>
+               <div className="h-full flex-1  flex justify-center items-center text-8xl ">
+                   <button className="w-auto h-auto cursor-pointer">
+                       <div>
+                           <i className="ri-money-dollar-circle-line"></i>
+                           <p className="underline text-2xl">Manage Payments</p>
+                       </div>
+                   </button>
+               </div>
            </div>
        </div>
+
+
     );
 }
